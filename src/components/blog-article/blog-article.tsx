ece888d5e6e07224a4551189aa5bfeeab2adb5a4
Alex Gorodov <a.gorodov.22@gmail.com>
2024-02-12 17:58:29 +0200

    subscribe

--- src/components/blog-article/blog-article.tsx
+++ src/components/blog-article/blog-article.tsx
@@ -5,7 +5,7 @@ import { AppRoute } from "../../const"
 export function BlogArticle(): JSX.Element {
   return (
     <article className="blog">
-      <Link className="link" to={AppRoute.Blog}>
+      <Link className="blog__link" to={AppRoute.Blog}>
         <span className="blog__subtitle">New in our blog</span>
         <h3 className="blog__title title title--3">10 ways to serve fruit sorbets to the table</h3>
       </Link>
--- src/components/subscribe-article/subscribe-article.tsx
+++ src/components/subscribe-article/subscribe-article.tsx
@@ -1,15 +1,16 @@
 import "../../styles/blocks/subscribe.sass"
+import "../../styles/blocks/subscribe-form.sass"
 
 export function SubscribeArticle(): JSX.Element {
   return (
     <article className="subscribe">
       <div className="subscribe__wrapper">
         <p className="subscribe__description">Subscribe to our sweet newsletter and always be keep abreast of all the delicious things that happen here. We promise not to spam or send any unnecessary nonsense. Honestly =)</p>
-        <form className="subscribe__form" action="" method="post">
-          <label htmlFor="email">
-            <input type="email" name="e-mail" id="email" placeholder="email@example.com"/>
+        <form className="subscribe__form subscribe-form" action="" method="post">
+          <label className="subscribe-form__input-wrapper" htmlFor="email">
+            <input className="subscribe-form__input" type="email" name="e-mail" id="email" placeholder="email@example.com"/>
           </label>
-          <button type="submit">Send</button>
+          <button className="subscribe-form__btn button" type="submit">Send</button>
         </form>
       </div>
     </article>
