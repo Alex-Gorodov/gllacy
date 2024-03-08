d26118d4f5e2982287ac352ba8cf68f7a1ab7cff
Alex Gorodov <a.gorodov.22@gmail.com>
2024-03-09 01:59:43 +0200

    add link to homepage from 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import '../../styles/blocks/not-found.sass'
+import { AppRoute } from '../../const'
 
 export function NotFoundPage() {
   return (
@@ -6,6 +8,7 @@ export function NotFoundPage() {
       <p className='not-found__text'>
         <span>404</span>
         <span>Page Not Found</span>
+        <Link className="not-found__link" to={AppRoute.Root}>To homepage</Link>
       </p>
     </div>
   )
