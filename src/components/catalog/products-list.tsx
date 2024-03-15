d2e6697b99d7364c3ce8c171f1b6a5360497dff0
Alex Gorodov <a.gorodov.22@gmail.com>
2024-03-15 19:28:32 +0200

    catalog pages

--- src/components/catalog/products-list.tsx
+++ src/components/catalog/products-list.tsx
@@ -11,23 +11,20 @@ import { AppRoute } from '../../const';
 export function ProductsList(): JSX.Element {
 
   const [page, setPage] = useState(1);
-  const [iceCreamType, setIceCreamType] = useState('all');
+  const [iceCreamType, setIceCreamType] = useState(IceCreamTypes.All);
   const [shownGoods, setShownGoods] = useState(12);
-  const [pageNumber, setPageNumber] = useState(Math.round(shopItems.length / 12));
-  const pagesAmount = Array.from({ length: pageNumber }, (_, index) => index + 1);
+  const [pagesNum, setPagesNum] = useState(Math.round(shopItems.length / shownGoods));
+  const pagesAmount = Array.from({ length: Math.ceil(iceCreamType === IceCreamTypes.All ? shopItems.length / shownGoods : pagesNum) }, (_, index) => index + 1);
   const iceCreamTypesArray: IceCreamTypes[] = Object.values(IceCreamTypes);
-  const changeTypeHandler = (type: string) => {
+
+  const changeTypeHandler = (type: IceCreamTypes) => {
     setIceCreamType(type);
     setPage(1);
-    setPageNumber(
-      type === 'all'
-        ?
-        Math.ceil(shopItems.length / 12)
-        :
-        Math.ceil(shopItems.filter((item) => item.type === type).length / 12)
-    )
-  }
-  
+    setShownGoods(12);
+    const filteredItems = shopItems.filter((item) => item.type === type);
+    setPagesNum(Math.ceil(filteredItems.length / shownGoods));
+  };
+
   return (
     <>
       <>
@@ -36,7 +33,7 @@ export function ProductsList(): JSX.Element {
             <Link className="breadcrumbs__link" to={AppRoute.Root}>Main</Link>
           </li>
           <li className="breadcrumbs__item">
-            <Link className="breadcrumbs__link" to={AppRoute.Catalog} onClick={() => setIceCreamType('')}>Catalog</Link>
+            <Link className="breadcrumbs__link" to={AppRoute.Catalog} onClick={() => setIceCreamType(IceCreamTypes.All)}>Catalog</Link>
           </li>
           <li className="breadcrumbs__item">
             <Link className="breadcrumbs__link breadcrumbs__link--active" to="#">{iceCreamType}</Link>
@@ -62,7 +59,7 @@ export function ProductsList(): JSX.Element {
       </>
       <ul className="products__list">
         {
-          iceCreamType === 'all'
+          iceCreamType === IceCreamTypes.All
           ?
           shopItems.slice(0 + shownGoods * (page - 1), shownGoods * page).map((item) => {
             return (
@@ -80,7 +77,19 @@ export function ProductsList(): JSX.Element {
       {
         shownGoods &&
         <div className="catalog__buttons-wrapper">
-          <button className="button button--white" disabled={shownGoods >= shopItems.length - 1} onClick={() => {setShownGoods(shownGoods + 12); setPageNumber(Math.floor(pagesAmount.length - 1))}}>Show more</button>
+          <button
+            className="button button--white"
+            disabled={pagesAmount.length < 2}
+            onClick={() => {
+              setShownGoods(shownGoods + 12);
+              const filteredItems = shopItems.filter((item) => item.type === iceCreamType);
+              console.log(filteredItems.length);
+              
+              setPagesNum(Math.ceil(filteredItems.length / (shownGoods + 12)));
+            }}
+          >
+            Show more
+          </button>
           <ul className="catalog-pagination">
 
             <li>
@@ -88,20 +97,26 @@ export function ProductsList(): JSX.Element {
                 <SliderArrowPrev/>
               </button>
             </li>
-            {pagesAmount.map((pageNumber) => {
-              const paginationBtnClassName = cn('catalog-pagination__btn', {
-                'catalog-pagination__btn--active' : page === pageNumber
-              });
-              return (
-                <li key={pageNumber}>
-                  <button className={paginationBtnClassName} data-pagination={pageNumber} onClick={() => setPage(pageNumber)}>
-                    {pageNumber}
-                  </button>
-                </li>
-              );
-            })}
+            {
+              pagesAmount.map((pageNumber) => {
+                const paginationBtnClassName = cn('catalog-pagination__btn', {
+                  'catalog-pagination__btn--active': page === pageNumber
+                });
+                return (
+                  <li key={`page-${pageNumber}`}>
+                    <button
+                      className={paginationBtnClassName}
+                      data-pagination={pageNumber}
+                      onClick={() => setPage(pageNumber)}
+                    >
+                      {pageNumber}
+                    </button>
+                  </li>
+                );
+              })
+            }
             <li>
-              <button className="catalog-pagination__btn" onClick={() => setPage(page + 1)} disabled={page >= pageNumber}>
+              <button className="catalog-pagination__btn" onClick={() => setPage(page + 1)} disabled={page >= pagesNum}>
                 <SliderArrowNext/>
               </button>
             </li>
