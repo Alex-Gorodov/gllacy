758a491f0261434d0bca224bbdf657c50a3d7d9e
Alex Gorodov <a.gorodov.22@gmail.com>
2024-03-28 04:20:00 +0200

    changes on main page slider

--- src/components/slider/slide-item/slide-item.tsx
+++ src/components/slider/slide-item/slide-item.tsx
@@ -21,38 +21,40 @@ type SlideProps = {
 export function SlideItem({slide}: SlideProps): JSX.Element {
   const isSliderMoving = useSelector((state: RootState) => state.slider.isMoving);
   const dispatch = useDispatch();
-  const activeSlide = useSelector((state: RootState) => state.slider.activeSlide);
+  const activeSlide = useSelector((state: RootState) => state.slider.activeSlide)
 
-  const imageClassName = cn('slide__image', {
-    'slide__image--loading': isSliderMoving,
+  const loading = cn({
+    'loading': isSliderMoving,
   });
 
   return (
     <div className="slider__item slide">
-      <h2 className="slide__title">{slide.title}</h2>
-      <p className="slide__description">{slide.description}</p>
-      <button className="slide__add-to-cart-btn button button--white" onClick={() => dispatch(addToCart({item: shopItems[activeSlide]}))}>Order</button>
-      <div className='slide__image-wrapper'>
-        <picture>
-          <source className={imageClassName} srcSet={`${slide.image}.webp`} type="image/webp" width={306} height={507}/>
-          <source media="(min-width: 1170px)" srcSet={`${slide.image}.webp 1x, ${slide.image}@2x.webp 2x`} type="image/webp"/>
-          <source media="(min-width: 900px)" srcSet={`${slide.image}.webp 1x, ${slide.image}@2x.webp 2x`} type="image/webp"/>
-          <img src={`${slide.image}.png`}  alt={slide.title}/>
-        </picture> 
-        <div className="slider__buttons">
-          <button className="slider__btn button button--circle" onClick={() => dispatch(setActiveSlide({activeSlide: activeSlide - 1}))}>
-            <ArrowPrev/>
-            <span className="visually-hidden">previous slide</span>
-          </button>
-          <button className="slider__btn button button--circle" onClick={() => dispatch(setActiveSlide({activeSlide: activeSlide + 1}))}>
-            <ArrowNext/>
-            <span className="visually-hidden">next slide</span>
-          </button>
+      <div className="slide__wrapper">
+        <h2 className={`slide__title ${loading}`}>{slide.title}</h2>
+        <p className={`slide__description ${loading}`}>{slide.description}</p>
+        <button className="slide__add-to-cart-btn button button--white" onClick={() => dispatch(addToCart({item: shopItems[activeSlide]}))}>Order</button>
+        <div className='slide__image-wrapper'>
+          <picture className={`${loading}`}>
+            <source className={`slide__image ${loading}`} srcSet={`${slide.image}.webp`} type="image/webp" width={306} height={507}/>
+            <source media="(min-width: 1170px)" srcSet={`${slide.image}.webp 1x, ${slide.image}@2x.webp 2x`} type="image/webp"/>
+            <source media="(min-width: 900px)" srcSet={`${slide.image}.webp 1x, ${slide.image}@2x.webp 2x`} type="image/webp"/>
+            <img src={`${slide.image}.png`}  alt={slide.title}/>
+          </picture>
+          <div className="slider__buttons">
+            <button className="slider__btn button button--circle" onClick={() => dispatch(setActiveSlide({activeSlide: activeSlide - 1}))}>
+              <ArrowPrev/>
+              <span className="visually-hidden">previous slide</span>
+            </button>
+            <button className="slider__btn button button--circle" onClick={() => dispatch(setActiveSlide({activeSlide: activeSlide + 1}))}>
+              <ArrowNext/>
+              <span className="visually-hidden">next slide</span>
+            </button>
+          </div>
         </div>
-        <div className="slider__next-slides">
-          <InactiveSlide onChangeSlide={() => dispatch(setActiveSlide({ activeSlide: activeSlide < sliderItems.length - 1 ? sliderItems[activeSlide + 1].id : sliderItems[0].id }))} slide={activeSlide < sliderItems.length - 1 ? sliderItems[activeSlide + 1] : sliderItems[0]} />
-          <InactiveSlide onChangeSlide={() => dispatch(setActiveSlide({ activeSlide: activeSlide < sliderItems.length - 2 ? sliderItems[activeSlide + 2].id : sliderItems[activeSlide - 1].id }))} slide={activeSlide < sliderItems.length - 2 ? sliderItems[activeSlide + 2] : sliderItems[activeSlide - 1]} />
-        </div>
+      </div>
+      <div className="slider__next-slides">
+        <InactiveSlide onChangeSlide={() => dispatch(setActiveSlide({ activeSlide: activeSlide < sliderItems.length - 1 ? sliderItems[activeSlide + 1].id : sliderItems[0].id }))} slide={activeSlide < sliderItems.length - 1 ? sliderItems[activeSlide + 1] : sliderItems[0]} />
+        <InactiveSlide onChangeSlide={() => dispatch(setActiveSlide({ activeSlide: activeSlide < sliderItems.length - 2 ? sliderItems[activeSlide + 2].id : sliderItems[activeSlide - 1].id }))} slide={activeSlide < sliderItems.length - 2 ? sliderItems[activeSlide + 2] : sliderItems[activeSlide - 1]} />
       </div>
       {
         useResizeListener() < MOBILE_WIDTH
--- src/store/middlewares/delay.ts
+++ src/store/middlewares/delay.ts
@@ -3,13 +3,10 @@ import { setActiveSlide, setSliderMoving } from "../slider/slider-actions";
 
 export const delay: Middleware = (store) => (next) => (action) => {
   if (setActiveSlide.match(action)) {
-    // Добавляем экшен setSliderMoving с задержкой 500 миллисекунд
-    // setTimeout(() => {
       store.dispatch(setSliderMoving({ isMoving: true }));
       setTimeout(() => {
         store.dispatch(setSliderMoving({ isMoving: false }));
-      }, 300);
-    // }, 500);
+      }, 700);
   }
 
   return next(action);
